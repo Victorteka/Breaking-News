c96777e10a75f88a60a6f7a150de0c0b3c5578c6
Victor Teka <ambukateka@gmail.com>
2021-02-23 14:13:38 +0300

    Add gradle management with buildSrc

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,21 @@
+object Versions {
+    const val kotlin = "1.4.20"
+    const val coreKtx = "1.3.2"
+    const val appCompat = "1.2.0"
+    const val material = "1.3.0"
+    const val constraint = "2.0.4"
+    const val junit = "4.+"
+    const val androidxJunit = "1.1.2"
+    const val espresso = "3.3.0"
+}
+
+object Libraries {
+    const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+    const val material = "com.google.android.material:material:${Versions.material}"
+    const val constraint = "androidx.constraintlayout:constraintlayout:${Versions.constraint}"
+    const val junit = "junit:junit:${Versions.junit}"
+    const val androidxJunit = "androidx.test.ext:junit:${Versions.androidxJunit}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+}
